dd83cf0cd8c3676c4a608fd909c87cc921057ff1
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-11 13:22:55 +0200

    add TransactionHistory component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from './Profile/user.json';
 import data from './Statistics/data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired))
+    .isRequired,
+};
