f25373bd1bc3c66336f33f2834c0e0b098dc195e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-14 13:58:20 +0200

    add styles for TransactionHistory component

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,32 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
 export const TransactionHistory = ({ items }) => {
+  const {
+    'transaction-history': table,
+    'table-head': tableHead,
+    'table-body': tableBody,
+    'table-header': tableHeader,
+    'table-data': tableData,
+    'table-row': tableRow,
+  } = css;
+
   return (
-    <table className="transaction-history">
-      <thead>
+    <table className={table}>
+      <thead className={tableHead}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={tableHeader}>Type</th>
+          <th className={tableHeader}>Amount</th>
+          <th className={tableHeader}>Currency</th>
         </tr>
       </thead>
 
-      <tbody>
+      <tbody className={tableBody}>
         {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+          <tr key={id} className={tableRow}>
+            <td className={tableData}>{type}</td>
+            <td className={tableData}>{amount}</td>
+            <td className={tableData}>{currency}</td>
           </tr>
         ))}
       </tbody>
