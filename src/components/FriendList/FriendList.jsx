1dae3c48f14d31f727a4c10509c8cb768a675c1d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-14 13:57:29 +0200

    add styles for FriendList & FriendListItem components

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
 import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css['friend-list']}>
       {friends.map(friend => (
         <FriendListItem
           key={friend.id}
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,14 +1,18 @@
+import css from './FriendListItem.module.css';
+
 export const FriendListItem = ({ avatar, name, isOnline }) => {
+  const { item, avatar: avatarStyles, name: nameStyles } = css;
+
   return (
-    <li className="item">
-      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+    <li className={item}>
+      <span className={isOnline ? css['is-online'] : css['is-offline']}></span>
       <img
-        className="avatar"
+        className={avatarStyles}
         src={avatar}
         alt={`${name}'s avatar`}
         width="48"
       />
-      <p className="name">{name}</p>
+      <p className={nameStyles}>{name}</p>
     </li>
   );
 };
